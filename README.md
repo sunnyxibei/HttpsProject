5f2a5d90ce616ef217f02ce7183535156532d0fe
sunnyxibei <sunnyxibei@foxmail.com>
2018-07-21 23:08:22 +0800

    Finish HTTPS connection according to the google developer website

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # HttpsProject
-手动配置HTTPS证书，建立HTTPS连接
+最近在学习HTTPS的原理以及HTTPS建立连接的流程，趁热打铁，在Android端撸了一个HTTPS证书配置的过程，以加深对HTTPS的理解。
 
 
 
-服务器环境使用Tomcat 9，配置了RSA证书
+服务器环境使用Tomcat 9，配置了RSA证书，放了一个json文件用来试验。
+
+
+
+Android端的代码是根据Google Developer一步一步学习的，官方文档非常翔实，多看有益。
--- app/src/main/java/cn/com/timeriver/httpsproject/MainActivity.kt
+++ app/src/main/java/cn/com/timeriver/httpsproject/MainActivity.kt
@@ -2,15 +2,17 @@ package cn.com.timeriver.httpsproject
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.TextUtils
 import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.AnkoLogger
-import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.*
 import org.jetbrains.anko.sdk25.coroutines.onClick
-import org.jetbrains.anko.toast
-import org.jetbrains.anko.warn
 import java.io.ByteArrayOutputStream
 import java.net.URL
+import java.security.KeyStore
+import java.security.cert.CertificateFactory
 import javax.net.ssl.HttpsURLConnection
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManagerFactory
 
 /**
  * 手动配置HTTPS证书，发送HTTPS请求
@@ -27,20 +29,54 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
     }
 
     private fun sendHttpsRequest() {
-        toast("发送HTTPS请求")
-        val uri = "https://192.168.31.139:8443/alipay.json"
+        toast("Send HTTPS Request")
+        val uri = "https://192.168.31.139:8443/tomcat.json"
         doAsync {
-            val openConnection = URL(uri).openConnection() as HttpsURLConnection
-            val inputStream = openConnection.inputStream
+            // Load CAs from an InputStream
+            // (could be from a resource or ByteArrayInputStream or ...)
+            val cf = CertificateFactory.getInstance("X.509")
+            val certficate = cf.generateCertificate(assets.open("tomcat.cert"))
+
+            // Create a KeyStore containing our trusted CAs
+            val keyStore = KeyStore.getInstance(KeyStore.getDefaultType())
+            // Clear keystore
+            keyStore.load(null)
+            keyStore.setCertificateEntry("tomcat", certficate)
+
+            // Create a TrustManager that trusts the CAs in our KeyStore
+            val tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
+            tmf.init(keyStore)
+
+            // Create an SSLContext that uses our TrustManager
+            val sslContext = SSLContext.getInstance("TLS")
+            sslContext.init(null, tmf.trustManagers, null)
+
+            // Tell the URLConnection to use a SocketFactory from our SSLContext
+            val connection = URL(uri).openConnection() as HttpsURLConnection
+            connection.sslSocketFactory = sslContext.socketFactory
+            // Create an HostnameVerifier that hardwires the expected hostname.
+            // Note that is different than the URL's hostname:
+            // example.com versus example.org
+            // Tell the URLConnection to use our HostnameVerifier
+            connection.setHostnameVerifier { hostname, session ->
+                TextUtils.equals("192.168.31.139", hostname)
+            }
+
+            val inputStream = connection.inputStream
             val buffer = ByteArray(1024 * 8)
-            var length = 0
             val baos = ByteArrayOutputStream()
+            var length = inputStream.read(buffer)
             while (length != -1) {
-                length = inputStream.read(buffer)
                 baos.write(buffer, 0, length)
+                length = inputStream.read(buffer)
             }
             baos.flush()
-            warn { baos.toString() }
+            val result = baos.toString()
+            warn { result }
+            uiThread {
+                toast("HTTPS Request Success")
+                toast(result)
+            }
         }
     }
 }
